35ce68403c5001e8a0df84b84a02193107141875
Shweta Sharma <shwetasharmaeco@gmail.com>
2020-05-20 05:49:03 +0000

    completed

---
+++ melonCounting.js
@@ -0,0 +1,27 @@
+melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+
+
+function countMelons(melon_list){
+    const melon_counts = {};
+
+    for (const melon of melon_list){
+        if (melon in melon_counts){
+            melon_counts[melon] += 1;
+        }
+        else{
+            melon_counts[melon] = 1;
+        }
+    }
+    return melon_counts
+}
